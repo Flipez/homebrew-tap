099d3f7890ebb00bf4ae1f2096688f56276586c2
Flipez <code@brauser.io>
2021-09-28 22:12:08 +0200

    Brew formula update for rocket-lang version v0.9.6-rc2

--- Formula/rocket-lang.rb
+++ Formula/rocket-lang.rb
@@ -5,21 +5,21 @@
 class RocketLang < Formula
   desc ""
   homepage "https://auch.cool/"
-  version "0.9.6-rc1"
+  version "0.9.6-rc2"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/Flipez/rocket-lang/releases/download/v0.9.6-rc1/rocket-lang_darwin_amd64.tar.gz"
-      sha256 "604ce670f0b3076e234af11705f716d60cf2ac57a0ba4f149d714562b0f5a4d5"
+      url "https://github.com/Flipez/rocket-lang/releases/download/v0.9.6-rc2/rocket-lang_darwin_amd64.tar.gz"
+      sha256 "803540047438baaa1cdab06d86a6fe877df386ee425af5f56fe4bc2b43358703"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/Flipez/rocket-lang/releases/download/v0.9.6-rc1/rocket-lang_linux_amd64.tar.gz"
-      sha256 "c227f6dbdd648f8f5400fb5b9e82d3f87aaaf738c278290aa2dbb0ec22c12b15"
+      url "https://github.com/Flipez/rocket-lang/releases/download/v0.9.6-rc2/rocket-lang_linux_amd64.tar.gz"
+      sha256 "bba1ffc9e30e22d986320572d8d9e439ac5aebaa5d5c8a9e178480063d16cf68"
     end
   end
 
